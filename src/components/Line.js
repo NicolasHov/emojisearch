6e7169489384102f4c45cc0586696cd56d37a9c6
Kevin <kfachas@gmail.com>
2021-07-22 18:27:50 +0200

    add navigatorclipboard for symbol

--- src/components/Line.js
+++ src/components/Line.js
@@ -3,7 +3,7 @@ import emojis from "../emojis.json";
 const Line = ({ title, symbol }) => {
   return (
     <ul class="Line">
-      <li onClick="">
+      <li onClick={() => navigator.clipboard.writeText(symbol)}>
         <p>{title}</p>
         <p>{symbol}</p>
       </li>
